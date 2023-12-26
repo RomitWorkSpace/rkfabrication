e62da23ad39a5ce4d99a332b63c7226491492f86
RomitWorkSpace <romitkumar848@gmail.com>
2023-12-26 10:53:36 +0530

    Blog card updated

--- resources/js/components/BlogCard.jsx
+++ resources/js/components/BlogCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const BlogCard=({props})=>{
     return(
         <>
            <div className="col-md-4 mb-4">
               <div className="blog-card">
-                 <img src={props.Blog_img} width="100%" alt="Blog image" />
-                 <h3>{ props.Title }</h3>
-                 <p>{ props.Desc }</p>
-                 <button className="rk-btn">Read More</button>
+                 <img src={props.image} width="100%" alt="Blog image" />
+                 <h3>{ props.title }</h3>
+                 <p>{ props.description }</p>
+                 <Link to={`/blog/${props.slug}`}>
+                     <button className="rk-btn">Read More</button>
+                 </Link>
               </div>
            </div>
         </>
     );
-}
+} 
 
 export default BlogCard;
